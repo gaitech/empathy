4cc82073a345e600eea48d990c7b2394fe3bd6f7
Harjeet <hs529771@gmail.com>
2018-02-15 17:39:04 +0530

    dynamic

--- wp-content/themes/empathy/functions.php
+++ wp-content/themes/empathy/functions.php
@@ -4,3 +4,16 @@ add_action( 'wp_enqueue_scripts', 'empathy_enqueue_child_styles', 99);
 function empathy_enqueue_child_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+function empathy_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Home Block', 'twentyseventeen' ),
+		'id'            => 'sidebar-4',
+		'description'   => __( 'Add widgets here to appear in your sidebar on blog posts and archive pages.', 'twentyseventeen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		// 'before_title'  => '<h2 class="widget-title">',
+		// 'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'empathy_widgets_init' );
--- wp-content/themes/empathy/index.php
+++ wp-content/themes/empathy/index.php
@@ -21,7 +21,7 @@ get_header(); ?>
 	<div class="container">
 		<div class="navigation">
 			<div class="site_logo">
-			<img src="<?php echo home_url(); ?>/wp-content/uploads/2018/02/logo.jpg">
+				<img src="<?php echo home_url(); ?>/wp-content/uploads/2018/02/logo.jpg">
 			</div>
 			<div class="navbar">
 				<ul class="menu">
@@ -42,108 +42,64 @@ get_header(); ?>
 			</div>
 		</section>
 		<section class="countries_real__stories">
-			<div class="real_stories">
-				<figure>
-					<img src="<?php echo home_url(); ?>/wp-content/uploads/2018/02/boy-256.png">
-				</figure>
-				<h1>Guru Singer</h1>
-				<p><a href="#">Psycology</a></p>
-				<p>JANUARY 2.2018</p>
-				<p class="content">Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-					Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-					Loren ipsum Loren ipsum Loren ipsum</p>
-					<div class="read_more">
-						<button>READ MORE</button>
-					</div>
-				</div>
+			<?php $query = new WP_Query( array( 'post_type' => 'story', 'order'=>'DESC', 'posts_per_page'=>-1, 'paged' => $paged ) );
+
+			foreach ($query->posts as $key => $value) {
+				$name = $value->post_title;
+				$id = $value->ID;
+				$content = $value->post_content;
+				$url = get_post_permalink( $id);
+				$image = wp_get_attachment_url( get_post_thumbnail_id( $id ) );
+				$custom_cont = get_post_meta($id, '', TRUE);
+				$field_of_work = $custom_cont['field_of_work'][0];
+				$country = $custom_cont['country'][0];
+				$website_one = $custom_cont['website_one'][0];
+				$website_two = $custom_cont['website_two'][0];
+				$website_three = $custom_cont['website_three'][0];
+				$organization_name = $custom_cont['organization_name'][0];
+				$empathetic_leadership = $custom_cont['empathetic_leadership'][0];
+				$purpose_of_orgnization = $custom_cont['purpose_of_orgnization'][0];
+				$video_section_one = $custom_cont['video_section_one'][0];
+				$video_section_two = $custom_cont['video_section_two'][0];
+				$video_section_three = $custom_cont['video_section_three'][0];
+				$reference_article_title_one = $custom_cont['reference_article_title_one'][0];
+				$reference_article_body_one = $custom_cont['reference_article_body_one'][0];
+				$reference_article_title_two = $custom_cont['reference_article_title_two'][0];
+				$reference_article_body_two = $custom_cont['reference_article_body_two'][0];
+				$contributor_name = $custom_cont['contributor_name'][0];
+				$contributor_description = $custom_cont['contributor_description'][0];
+				$soundcloud_url = $custom_cont['soundcloud_url'][0];
+
+				?>
 				<div class="real_stories">
 					<figure>
-						<img src="<?php echo home_url(); ?>/wp-content/uploads/2018/02/boy-256.png">
+						<img src="<?php echo $image ?>">
 					</figure>
-					<h1>Guru Singer</h1>
-					<p><a href="#">Psycology</a></p>
-					<p>JANUARY 2.2018</p>
-					<p class="content">Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-						Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-						Loren ipsum Loren ipsum Loren ipsum</p>
-						<div class="read_more">
-							<button>READ MORE</button>
-						</div>
+					<h1><?php echo $name; ?></h1>
+					<p><a href="#"><?php echo $field_of_work; ?></a></p>
+					<!-- <p>JANUARY 2.2018</p> -->
+					<p class="content"><?php $des = substr($content, 0 , 250); echo $des; ?></p>
+					<div class="read_more">
+						<button><a href="<?php echo esc_url( get_permalink( $id ) ); ?>">READ MORE</a></button>
 					</div>
-					<div class="real_stories">
-						<figure>
-							<img src="<?php echo home_url(); ?>/wp-content/uploads/2018/02/boy-256.png">
-						</figure>
-						<h1>Guru Singer</h1>
-						<p><a href="#">Psycology</a></p>
-						<p>JANUARY 2.2018</p>
-						<p class="content">Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-							Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-							Loren ipsum Loren ipsum Loren ipsum</p>
-							<div class="read_more">
-								<button>READ MORE</button>
-							</div>
-						</div>
-					</section>
-					<section class="countries_real__stories">
-						<div class="real_stories">
-							<iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY">
-							</iframe>
-							<h1>Guru Singer</h1>
-							<p><a href="#">Psycology</a></p>
-							<p>JANUARY 2.2018</p>
-							<p class="content">Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-								Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-								Loren ipsum Loren ipsum Loren ipsum</p>
-								<div class="read_more">
-									<button>READ MORE</button>
-								</div>
-							</div>
-							<div class="real_stories">
-								<figure>
-									<img src="<?php echo home_url(); ?>/wp-content/uploads/2018/02/boy-256.png">
-								</figure>
-								<h1>Guru Singer</h1>
-								<p><a href="#">Psycology</a></p>
-								<p>JANUARY 2.2018</p>
-								<p class="content">Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-									Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-									Loren ipsum Loren ipsum Loren ipsum</p>
-									<div class="read_more">
-										<button>READ MORE</button>
-									</div>
-								</div>
-								<div class="real_stories">
-									<figure>
-										<img src="<?php echo home_url(); ?>/wp-content/uploads/2018/02/boy-256.png">
-									</figure>
-									<h1>Guru Singer</h1>
-									<p><a href="#">Psycology</a></p>
-									<p>JANUARY 2.2018</p>
-									<p class="content">Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-										Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-										Loren ipsum Loren ipsum Loren ipsum</p>
-										<div class="read_more">
-											<button>READ MORE</button>
-										</div>
-									</div>
-								</section>
-							</div>
-							<div class="about_us__section">
-								<div class="container">
-									<section class="about_empathy">
-										<p>Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-											Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-											Loren ipsum Loren ipsum Loren ipsumLoren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-											Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-											Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-											Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-											Loren ipsum Loren ipsum Loren ipsumLoren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-											Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
-											Loren ipsum Loren ipsum Loren ipsum</p>
-										</section>
-									</div>
-								</div>
-							</div>
+				</div>
+				<?php } ?>
+			</section>
 
-							<?php get_footer();
+		</div>
+		<div class="about_us__section">
+			<div class="container">
+				<section class="about_empathy">
+				<p>
+					<?php if ( is_active_sidebar( 'sidebar-4' ) ) { ?>
+			           <div class="widget-column footer-widget-4">
+				           <?php dynamic_sidebar( 'sidebar-4' ); ?>
+			           </div>
+		            <?php } ?>
+		        </p>
+				</section>
+			</div>
+		</div>
+	</div>
+
+	<?php get_footer();
