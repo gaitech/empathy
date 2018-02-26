b0976fd69263a659fd96ceda1bf68664e74e5875
Harjeet <hs529771@gmail.com>
2018-02-26 16:41:52 +0530

    modified function and style file

--- wp-content/themes/empathy/functions.php
+++ wp-content/themes/empathy/functions.php
@@ -59,6 +59,16 @@ function empathy_widgets_init() {
 	    'before_title'  => '<h2 class="widget-title">',
 	    'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Detail Text Block', 'twentyseventeen' ),
+		'id'            => 'sidebar-8',
+		'description'   => __( 'Add widgets here to appear in your sidebar on blog posts and archive pages.', 'twentyseventeen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+	    'before_title'  => '<h2 class="widget-title">',
+	    'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'empathy_widgets_init' );
 
