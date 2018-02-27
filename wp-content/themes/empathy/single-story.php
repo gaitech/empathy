34e5876e99dac16d5803d694bb5eac7f3333e5c8
guru985 <abhay.rajput39@gmail.com>
2018-02-27 14:14:57 +0530

    changes in detail page

--- wp-content/themes/empathy/single-story.php
+++ wp-content/themes/empathy/single-story.php
@@ -22,6 +22,11 @@ get_header(); ?>
 
 				get_template_part( 'template-parts/content-story', get_post_format() );
 
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
 			endwhile; // End of the loop.
 			?>
 		</main><!-- #main -->
